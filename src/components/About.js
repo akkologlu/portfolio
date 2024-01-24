afa5a751136e722ba199571204c06f3392261d51
akkologlu <abdullahakkol@gmail.com>
2024-01-24 18:03:57 +0300

    about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,147 @@
 import React from "react";
+import aboutphotos from "../assets/aboutphotos.png";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaMedium,
+  FaInstagram,
+  FaXTwitter,
+  FaCodepen,
+  FaDiscord,
+  FaSpotify,
+} from "react-icons/fa6";
+import { SiBuymeacoffee } from "react-icons/si";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="flex xl:flex-row  w-full my-auto flex-col-reverse">
+      <div className="xl:w-2/5 xl:min-w-[510px]">
+        <img src={aboutphotos} alt="" />
+      </div>
+      <div className="xl:w-3/5 flex flex-col space-y-10 my-auto mx-auto">
+        <h1 className="text-5xl font-poppins text-gray-700 font-bold">
+          👀 About Me
+        </h1>
+        <p className="text-xl font-roboto text-gray-600 text-justify">
+          Hello, I am Abdullah. I was born in 2000 in Bolu, Turkey. After living
+          in Bolu for 19 years, I moved to Muğla for my university education. I
+          am currently a final year student in the Computer Engineering
+          department at Muğla Sıtkı Koçman University. I am developing projects
+          in the field of web development and especially working with MERN Stack
+          technologies. I am also learning blockchain technologies and taking
+          part in related projects. I am trying to explore the Web3 world and
+          aim to continue my career in these areas.
+        </p>
+        <p className="text-xl font-roboto text-gray-600 text-justify">
+          During my internship period, I worked at HAVELSAN, one of the leading
+          technology companies in Turkey. This experience was much more than
+          just an intern for me. By taking part in HAVELSAN's corporate culture,
+          I had the chance to play an active role in real projects. In this
+          process, I had the opportunity to improve my technical skills and
+          increase my cooperation and communication skills by taking
+          responsibility in a team.
+        </p>
+        <p className="text-xl font-roboto text-gray-600 text-justify">
+          My interest in technology and the experiences I gained during my
+          internship period motivate me even more. Taking part in creative
+          projects, mastering new technologies and shaping my career in this
+          field has become a lifestyle for me. I aim to take advantage of growth
+          opportunities by sharing the knowledge and experience I have gained
+          throughout my education life with you and signing more beautiful
+          projects together. Are you ready to join this journey with me? Let's
+          work together.
+        </p>
+        <div className="flex space-x-6 items-center">
+          <a
+            href="https://akkologluresume.s3.eu-north-1.amazonaws.com/akkologlu_cv.pdf"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 md:font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800;"
+          >
+            Resume
+          </a>
+          <a
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 md:font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800;"
+            href="mailto:abdullahakkol@gmail.com"
+          >
+            Contact
+          </a>
+          <a
+            href="https://www.linkedin.com/in/akkologlu/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-blue-700 text-white rounded-full p-2 hover:bg-white hover:text-blue-700 ease-in-out duration-200"
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://github.com/akkologlu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-black text-white rounded-full p-2 hover:bg-white hover:text-black ease-in-out duration-200"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://medium.com/@akkologlu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-black text-white rounded-full p-2 hover:bg-white hover:text-black ease-in-out duration-200"
+          >
+            <FaMedium />
+          </a>
+          <a
+            href="https://www.instagram.com/akkologluu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-red-600 text-white rounded-full p-2 hover:bg-white hover:text-red-600 ease-in-out duration-200"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.x.com/akkologluu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-black text-white rounded-full p-2 hover:bg-white hover:text-black ease-in-out duration-200"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href="https://www.buymeacoffee.com/akkologlu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-yellow-400 text-white rounded-full p-2 hover:bg-white hover:text-yellow-400 ease-in-out duration-200"
+          >
+            <SiBuymeacoffee />
+          </a>
+          <a
+            href="https://codepen.io/akkologlu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-black text-white rounded-full p-2 hover:bg-white hover:text-black ease-in-out duration-200"
+          >
+            <FaCodepen />
+          </a>
+          <a
+            href="discordapp.com/users/477908109013024768"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-[#5865f2] text-white rounded-full p-2 hover:bg-white hover:text-[#5865f2] ease-in-out duration-200"
+          >
+            <FaDiscord />
+          </a>
+          <a
+            href="https://open.spotify.com/user/akkologluu?si=666a92a60803446e"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl bg-[#1ED760] text-white rounded-full p-2 hover:bg-white hover:text-[#1ED760] ease-in-out duration-200"
+          >
+            <FaSpotify />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,11 +2,15 @@ import React from "react";
 import dvlimage from "../assets/dvlimage.png";
 import flypqs from "../assets/flypqs.png";
 import words from "../assets/2000words.png";
+import coverletter from "../assets/coverletter.png";
+import watchlyst from "../assets/watchlyst.png";
+import skyhub from "../assets/skyhub.png";
+import havelsan from "../assets/havelsan.jpg";
 import "../style/portfolio.css";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col space-y-12 mt-12 w-full ">
+    <div className="flex flex-col space-y-12 mt-12 w-full mb-12">
       <div className="portfolioRow">
         <div className="portfolioImg">
           <img src={dvlimage} alt="" />
@@ -98,6 +102,30 @@ function Portfolio() {
         </div>
       </div>
       <hr />
+      <div className="portfolioRow">
+        <div className="portfolioImg">
+          <img src={havelsan} alt="" />
+        </div>
+        <div className="portfolioContent">
+          <h1>Intership Task</h1>
+          <p className="title ">
+            During my internship at HAVELSAN A.Ş., I contributed to a digital
+            transformation project. I developed a system that allows a company
+            to view and organise personnel and project data through a management
+            panel. In this management panel, I designed an interface that lists
+            the details of projects and added sorting, filtering and file upload
+            features to improve data retrieval. I also created a multi-step form
+            to add new projects. The challenges I faced in the project included
+            file upload and profile photo upload processes using the FormData
+            function. Integrating the login page and authentication process into
+            the project also came with its own challenges, but I successfully
+            completed it by following these steps. I believe that the
+            experiences I gained in this project will form an important
+            foundation in my future professional life.
+          </p>
+        </div>
+      </div>
+      <hr />
       <div className="portfolioRow ">
         <div className="portfolioImg">
           <img src={words} alt="" />
@@ -105,16 +133,16 @@ function Portfolio() {
         <div className="portfolioContent">
           <h1>2000 Words</h1>
           <p className="title ">
-            FlyPqs is a dynamic web application that enables users to search and
-            list flights on specific dates and at airports. It stands out with
-            its intuitive interface and visual animation presented at first use.
-            Flight information is showcased with easy sorting and filtering
-            features, prioritizing user experience.
+            2000Words is a practical and interactive educational platform for
+            learning the basic 2000 words in the English language. Users can
+            easily learn and practice unfamiliar words with smart flash cards.
+            Enrich your language skills and make your communication more
+            effective with the power of 2000 words.
           </p>
           <div className="buttons">
             <div className="button">
               <a
-                href="https://flypqs.netlify.app/"
+                href="https://github.com/akkologlu/2000Words"
                 className="link flex items-center"
                 target="_blank"
                 rel="noreferrer"
@@ -139,7 +167,7 @@ function Portfolio() {
             </div>
             <div className="button">
               <a
-                href="https://github.com/akkologlu/FlightSearchApplication"
+                href="https://github.com/akkologlu/2000Words"
                 className="link flex items-center"
                 target="_blank"
                 rel="noreferrer"
@@ -184,6 +212,280 @@ function Portfolio() {
               alt="MongoDB Badge"
               className="badge"
             ></img>
+            <img
+              src="https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=tailwindcss&logoColor=fff&style=flat-square"
+              alt="Tailwind CSS Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/Axios-5A29E4?logo=axios&logoColor=fff&style=flat-square"
+              alt="Axios Badge"
+              className="badge"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="portfolioRow ">
+        <div className="portfolioImg">
+          <img src={coverletter} alt="" />
+        </div>
+        <div className="portfolioContent">
+          <h1>Cover Letter Generator</h1>
+          <p className="title ">
+            This project aims to develop a software application that provides
+            users with the opportunity to create a resume cover letter quickly
+            and effectively. After entering their personal information and
+            relevant details about the position they are applying for, users
+            will have a professionally crafted cover letter generated by
+            artificial intelligence.
+          </p>
+          <div className="buttons">
+            <div className="button">
+              <a
+                href="https://coverletteraigenerator.netlify.app/"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>DEMO</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="button">
+              <a
+                href="https://github.com/akkologlu/coverLetterAI"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>CODE</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="badges">
+            <img
+              src="https://img.shields.io/badge/OpenAI-412991?logo=openai&logoColor=fff&style=flat-square"
+              alt="OpenAI Badge"
+              className="badge"
+            ></img>
+            <img
+              src="https://img.shields.io/badge/React-61DAFB?logo=react&logoColor=000&style=flat-square"
+              alt="React Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=tailwindcss&logoColor=fff&style=flat-square"
+              alt="Tailwind CSS Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/Axios-5A29E4?logo=axios&logoColor=fff&style=flat-square"
+              alt="Axios Badge"
+              className="badge"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="portfolioRow ">
+        <div className="portfolioImg">
+          <img src={watchlyst} alt="" />
+        </div>
+        <div className="portfolioContent">
+          <h1>Watchlyst</h1>
+          <p className="title ">
+            Watchlyst, using the TMDB API, is a React-based platform allowing
+            users to easily create and manage personalized watchlists. Discover
+            the latest and popular movies, search for specific titles, and
+            access detailed information on individual movie pages. Tailwind CSS
+            ensures a sleek design. After watching, effortlessly remove movies
+            from your watchlist. Streamlining movie discovery and user control,
+            Watchlyst offers a concise and user-friendly experience.
+          </p>
+          <div className="buttons">
+            <div className="button">
+              <a
+                href="https://watchlyst.netlify.app/"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>DEMO</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="button">
+              <a
+                href="https://github.com/akkologlu/watchlist"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>CODE</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="badges">
+            <img
+              src="https://img.shields.io/badge/The%20Movie%20Database-01B4E4?logo=themoviedatabase&logoColor=fff&style=flat-square"
+              alt="The Movie Database Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/React-61DAFB?logo=react&logoColor=000&style=flat-square"
+              alt="React Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=tailwindcss&logoColor=fff&style=flat-square"
+              alt="Tailwind CSS Badge"
+              className="badge"
+            />
+            <img
+              src="https://img.shields.io/badge/Axios-5A29E4?logo=axios&logoColor=fff&style=flat-square"
+              alt="Axios Badge"
+              className="badge"
+            ></img>
+            <img
+              src="https://img.shields.io/badge/Swiper-6332F6?logo=swiper&logoColor=fff&style=flat-square"
+              alt="Swiper Badge"
+              className="badge"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="portfolioRow ">
+        <div className="portfolioImg">
+          <img src={skyhub} alt="" />
+        </div>
+        <div className="portfolioContent">
+          <h1>SkyHub</h1>
+          <p className="title ">
+            In this project, a user-friendly website has been crafted, allowing
+            users to seamlessly view the current weather page alongside location
+            information. The search page enables effortless checking of the
+            weather for any desired location. Importantly, cities can be
+            favorited, streamlining the tracking of weather information for
+            preferred locations
+          </p>
+          <div className="buttons">
+            <div className="button">
+              <a
+                href="https://skyhub.netlify.app/"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>DEMO</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="button">
+              <a
+                href="https://github.com/akkologlu/SkyHub"
+                className="link flex items-center"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>CODE</p>
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="badges">
+            <img
+              src="https://img.shields.io/badge/React-61DAFB?logo=react&logoColor=000&style=flat-square"
+              alt="React Badge"
+              className="badge"
+            />
             <img
               src="https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=tailwindcss&logoColor=fff&style=flat-square"
               alt="Tailwind CSS Badge"
@@ -282,7 +584,6 @@ function Portfolio() {
           </div>
         </div>
       </div>
-      <hr />
     </div>
   );
 }
